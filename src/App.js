201c61f6d3389259c9d3c179d43cd7e577a3ad0b
Pedro <haeser.pedro@gmail.com>
2020-04-07 03:45:51 -0300

    features list create delete repos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
+  const [repos, setRepos] = useState([])
+
+  useEffect(() => {
+    onLoad()
+  }, [])
+
+  async function onLoad() {
+    try {
+      const { data } = await api.get('repositories')
+      setRepos(data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const { data: newRepo } = await api.post('repositories', {
+        title: `From client: ${new Date()}`,
+        url: 'https://...url',
+        techs: ['Hello', 'from', 'React']
+      })
+
+      setRepos(prevRepos => [...prevRepos, newRepo])
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`)
+      const index = repos.findIndex(({ id: repoId }) => repoId == id)
+      setRepos([...repos.slice(0, index), ...repos.slice(index + 1)])
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+      <ul data-testid='repository-list'>
+        {repos.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
